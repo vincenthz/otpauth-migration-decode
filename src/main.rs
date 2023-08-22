82b20db081691fd94b181f486170d93970e3f046
Vincent Hanquez <vincent@coretype.io>
2023-08-22 16:25:48 +0700

    use a clap parser

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use base64::Engine as _;
+use clap::Parser;
 use prost::Message;
+use std::path::PathBuf;
 use url::Url;
 
 pub mod otpauth {
     include!(concat!(env!("OUT_DIR"), "/otp.migration.rs"));
 }
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the image file that contains a QR code
+    file: PathBuf,
+    #[arg(short, long)]
+    as_url: bool,
+}
+
 fn main() {
-    let args = std::env::args().collect::<Vec<_>>();
-    let loc = args[1].clone();
+    let args = Args::parse();
+
+    let loc = &args.file;
     let img = image::open(loc).unwrap();
 
     let decoder = bardecoder::default_decoder();
@@ -18,7 +30,7 @@ fn main() {
     let b = results[0].as_ref().expect("decoder QR code works").clone();
     let url = Url::parse(&b).expect("parsing decoded URL");
     if url.scheme() != "otpauth-migration" {
-        println!("wrong scheme for {}", url);
+        println!("wrong scheme, got {}", url);
         return;
     }
     match url.query_pairs().next() {
@@ -32,17 +44,22 @@ fn main() {
                 let b = bytes::Bytes::from(b);
                 let m = otpauth::MigrationPayload::decode(b).unwrap();
                 for params in m.otp_parameters {
-                    println!(
-                        "name: {} -- alg: {} -- digits: {} -- type: {} -- secret: {}",
-                        params.name,
-                        params.algorithm,
-                        params.digits,
-                        params.r#type,
-                        base32::encode(
-                            base32::Alphabet::RFC4648 { padding: false },
-                            &params.secret
+                    if args.as_url {
+                        todo!()
+                    } else {
+                        println!(
+                            "name: {} -- issuer: {} -- alg: {} -- digits: {} -- type: {} -- secret: {}",
+                            params.name,
+                            params.issuer,
+                            params.algorithm,
+                            params.digits,
+                            params.r#type,
+                            base32::encode(
+                                base32::Alphabet::RFC4648 { padding: false },
+                                &params.secret
+                            )
                         )
-                    )
+                    }
                 }
             }
         }
